c930475339f651d71d564dcf2352aa1c2067cc2d
Quentin Walker <quentin.walker@noaa.gov>
2026-04-29 16:08:46 -0400

    update click to crop

--- R/click_to_crop.R
+++ R/click_to_crop.R
@@ -15,42 +15,35 @@
 #' r <- rast(ncols=100, nrows=100, xmin=0, xmax=10, ymin=0, ymax=10)
 #' values(r) <- runif(ncell(r))
 #'
-#' # 2. Basic Crop & Mask (User draws a custom polygon)
+#' # 2. Basic Crop & Mask
 #' cropped_poly <- click_to_crop(r, mask = TRUE)
 #'
-#' # 3. Rectangular Bounding Box Crop (User clicks two points)
+#' # 3. Rectangular Bounding Box Crop
 #' cropped_bbox <- click_to_crop(r, mask = FALSE)
 #' }
 click_to_crop <- function(raster_obj, mask = TRUE) {
+  # Plot the original raster so you have something to click on
+  plot(raster_obj, main = "Click to define polygon (Esc when done)")
 
-  if (!inherits(raster_obj, "SpatRaster")) {
-    stop("Input must be a SpatRaster object.")
-  }
+  # Click points on the map; use right-click/Esc to close the shape
+  message("Click points on the plot. Finish by pressing 'Esc'.")
 
-  terra::plot(raster_obj, main = "Click to define area (Esc when done)")
+  poly <- terra::draw("polygon", col = 'red')
 
-  # Use 'polygon' for masking, or 'extent' for just the box
-  draw_mode <- if (mask) "polygon" else "extent"
-  message(paste0("Drawing mode: ", draw_mode, ". Click and press 'Esc' when finished."))
-
-  poly <- terra::draw(draw_mode, col = 'red')
-
-  if (is.null(poly)) {
-    message("No area selected; returning original.")
-    return(raster_obj)
-  }
+  message("polygon finished. cropping raster to polygon.")
 
   terra::crs(poly) <- terra::crs(raster_obj)
 
-  # Perform the crop
-  r_out <- terra::crop(raster_obj, poly)
-
-  # Perform the optional mask
-  if (mask) {
-    message("Masking raster to polygon shape...")
-    r_out <- terra::mask(r_out, poly)
+  # crop and mask
+  r_cropped <- terra::crop(raster_obj, poly)
+  if(mask){
+    r_final <- terra::mask(r_cropped, poly)
+  } else {
+    r_final <- r_cropped
   }
 
-  terra::plot(r_out, main = "Result")
-  return(r_out)
+  # Plot the result
+  terra::plot(r_final, main = "Cropped Result")
+
+  return(r_final)
 }
